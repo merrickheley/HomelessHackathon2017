27799669729b115a1b5a680d6f54fad14a201707
merrick.heley <merrick.heley@gmail.com>
2017-06-03 16:10:03 +1000

    Modify search algorithm so it actually works

--- HackApp/views.py
+++ HackApp/views.py
@@ -1,25 +1,29 @@
-import math
+import math, decimal
 
 from django.shortcuts import render, get_object_or_404, redirect
 from HackApp.models import *
 
-def MatchHouses(lat, long, accommodation_length=SHORTTERM, tenant_type=HOMELESS):
+def MatchHouses(lat, lon, accommodation_length=SHORTTERM, tenant_type=HOMELESS):
     # Filter based on essential criteria
-    houses = House.objects.get(accommodation_length=accommodation_length, tenant_type=tenant_type)
+    houses = House.objects.filter(accommodation_length=accommodation_length, tenant_type=tenant_type)
+
+    # No houses, no score sort
+    if len(houses) == 0:
+        return []
 
     for house in houses:
         # Initial score is based on absolute distance
-        house.score = math.sqrt((house.lat - lat)**2 + (house.long - long)**2)
+        house.score = math.sqrt((house.lat - decimal.Decimal(lat))**2 + (house.lon - decimal.Decimal(lon))**2)
 
         # Modify score by services available
-        services = AvailableService.objects.get(house=house)
+        services = AvailableService.objects.filter(house=house)
         for service in services:
             house.score -= service.weight
 
     # Sort based on location
+    houses = list(houses)
     houses.sort(key=lambda house: house.score)
-
-    return houses
+    return house
 
 # Create your views here.
 def basic(request):
