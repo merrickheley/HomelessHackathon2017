75e313f42708d75c29abb8e67ebdc2ce30843340
merrick.heley <merrick.heley@gmail.com>
2017-06-03 13:46:16 +1000

    Create the database models and migrate it

--- HackApp/models.py
+++ HackApp/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+class House(models.Model):
+    SHORTTERM = "ST"
+    LONGTERM = "LT"
+    ACCOMM_CHOICES = (
+        (SHORTTERM, 'Short Term'),
+        (LONGTERM, 'Long Term'),
+    )
+
+    HOMELESS = "HL"
+    ATRISK = "AR"
+    YOUTH = "YT"
+    TENANT_CHOICES = (
+        (HOMELESS, 'Homeless'),
+        (ATRISK, 'At Risk'),
+        (YOUTH, 'Youth'),
+    )
+
+    address = models.CharField(max_length=200)
+    post_code = models.CharField(max_length=6)
+    photo = models.CharField(max_length=50)
+    contact_name = models.CharField(max_length=30)
+    contact_phone = models.CharField(max_length=15)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    accomodation_length = models.CharField(
+        max_length=2,
+        choices=ACCOMM_CHOICES,
+        default=SHORTTERM,
+    )
+    tenant_type = models.CharField(
+        max_length=2,
+        choices=TENANT_CHOICES,
+        default=HOMELESS,
+    )
+
+class ServiceProvider(models.Model):
+    name = models.CharField(max_length=50)
+
+class Service(models.Model):
+    address = models.CharField(max_length=200)
+    post_code = models.CharField(max_length=6)
+    lat = models.DecimalField(max_digits=9, decimal_places=6)
+    lon = models.DecimalField(max_digits=9, decimal_places=6)
+    provider = models.ForeignKey('ServiceProvider')
+
+class AvailableServices(models.Model):
+    house = models.ForeignKey('House')
+    service = models.ForeignKey('Service')
+
