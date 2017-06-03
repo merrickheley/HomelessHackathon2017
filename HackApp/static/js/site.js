a075a9fe9c4172da0146b3cccd038cb71826d0e0
DESWIK\chris.ponticello <chris.ponticello@deswik.com>
2017-06-03 19:54:16 +1000

    Initial implementation of homepage wizard

--- HackApp/static/js/site.js
+++ HackApp/static/js/site.js
@@ -6,6 +6,33 @@ $(document).on('click', 'a.smooth', function(event){
     }, 500);
 });
 
+$(document).ready(function(){
+
+    $('#home-search button').click(function(ev) {
+        if ($(ev.target).hasClass('disabled')) return;
+
+        $(ev.target).closest('.form-group').parent().find('.form-group.p1').addClass('hidden');
+        $(ev.target).closest('.form-group').parent().find('.form-group.p2').removeClass('hidden');
+    });
+
+    $('#home-search select').on('input', function(ev){
+        $(ev.target).closest('.form-group').next('.form-group').find('button').removeClass('disabled');
+    });
+
+    $('select.deselected').on('input', function(ev){
+        $(ev.target).removeClass('deselected');
+    });
+
+    /*
+    $('#home-search input').on('input', function(ev){
+        $(ev.target).closest('.form-group').parent().find('.form-group').addClass('hidden');
+        $(ev.target).closest('.form-group').next('.form-group').removeClass('hidden');
+
+    });
+    */
+    //$(this).closest('.form-group').next('.form-group').removeClass('invisible');
+})
+
 //$("a[href='#bottom']").click(function() {
 //  $("html, body").animate({ scrollTop: $(document).height() }, "slow");
 //  return false;
