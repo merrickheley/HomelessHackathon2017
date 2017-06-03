df287cd13e2b9913d4ba0ee615aa3de180c838e4
merrick.heley <merrick.heley@gmail.com>
2017-06-03 14:50:37 +1000

    Fix admin console

--- HackApp/admin.py
+++ HackApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(House)
+admin.site.register(ServiceProvider)
+admin.site.register(Service)
+admin.site.register(AvailableService)
