21be2b86550217c246abcb06377f9bed15b80209
DESWIK\chris.ponticello <chris.ponticello@deswik.com>
2017-06-04 00:19:04 +1000

    Basic map injection, with templating

--- HackApp/static/js/site.js
+++ HackApp/static/js/site.js
@@ -1,3 +1,14 @@
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+
+// -------------------------------------------------------------------
+
 $(document).on('click', 'a.smooth', function(event){
     event.preventDefault();
 
