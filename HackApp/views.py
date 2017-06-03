fdabf424b85cf08878322bd14a8002f2a4d97966
DESWIK\chris.ponticello <chris.ponticello@deswik.com>
2017-06-03 13:42:51 +1000

    Django hello world

--- HackApp/views.py
+++ HackApp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+def basic(request):
+    return render(request, "HackApp/base.html")
